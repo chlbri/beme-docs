75e1f1d14348e77e192fb42b7624c5faba02e8a9
chlbri <51156116+chlbri@users.noreply.github.com>
2026-01-12 19:47:33 +0000

    feat(nav_beauty_hover): ajuster le rayon de bordure en fonction du support CSS
    _NO_CI

--- src/routes/components/nav_beauty_hover/-components.tsx
+++ src/routes/components/nav_beauty_hover/-components.tsx
@@ -25,7 +25,10 @@ export const NavBlurImage: Component<Props> = props => {
              & > nav {
                isolation: isolate;
                anchor-name: --hovered-link;
-               border-radius: 24px;
+
+               @supports not (corner-shape: squircle) {
+                 border-radius: 54px;
+               }
    
                @supports (corner-shape: squircle) {
                  border-radius: 54px;
